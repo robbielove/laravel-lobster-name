bfafabfffdeefd7dcfa055b5ae3fbd9f9f059123
Robbie Love <Robbie@ubiquilife.com>
2021-10-30 23:53:24 +1000

    update to run standalone second attempt

--- lobster-name.php
+++ lobster-name.php
@@ -3,12 +3,13 @@
 // take a first_name and use the first letter
 // to make a (L)obster name
 // e.g. Sammy Sobster
-public function getLobsterNameAttribute() {
-  $letter = substr($this->first_name, 0, 1);
-  return $this->first_name . ' ' . ucfirst($letter) . 'obster';
+
+public function getLobsterNameAttribute($name) {
+  $letter = substr($name, 0, 1);
+  return $name . ' ' . ucfirst($letter) . 'obster';
 }
 
 ?>
 
-    <h4>Lobster Name</h4>
-    <h4><?php echo getLobsterNameAttribute() ?></h4>
+<h4>Lobster Name</h4>
+<h4><?php echo getLobsterNameAttribute('Robbie') ?></h4>
